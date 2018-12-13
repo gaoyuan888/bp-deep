0beda7866c9ada5c5edc592eaf30b7648e03d2ed
yaoyizhou1 <yauri.zhang@qunar.com>
2018-12-13 20:48:19 +0800

    优化测试

--- src/main/java/cn/gaoyuan/core/App.java
+++ src/main/java/cn/gaoyuan/core/App.java
@@ -2,14 +2,11 @@ package cn.gaoyuan.core;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-       Integer a=null;
-       a=a==null?1:a;
+public class App {
+    public static void main(String[] args) {
+        Integer a = null;
+        a = a == null ? 1 : a;
         System.out.println(a);
     }
 }
